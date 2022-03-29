2c9fedbeb57891af6e759033970ea3caebd5690a
Daniil Panko <daniil.panko@mail.ru>
2022-03-30 00:13:48 +0300

    Refactor PageView

--- profiles/page_views.py
+++ profiles/page_views.py
@@ -1,6 +1,3 @@
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework import mixins, viewsets
 from profiles.permissions import PagePermission
 
